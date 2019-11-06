dc5b4630672946a88726c94c42ae5e4aae53fb3b
Omar <omar.c.lopez@hotmail.com>
2019-11-06 02:55:42 -0300

    hbs test

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
-const express = require('express');
-const port = 3000;
-const app = express();
+let express = require('express'),
+    path = require('path'),
+    port = 3000,
+    app = express();
 
-app.get('/', (req, res, next) => {
-    res.send({
-        title: "Meu primeiro servidor Express",
-        version: "0.0.0"
-    });
+//app.engine('.hbs', exphbs({extname: '.hbs'}));
+app.set('views', path.join(__dirname, 'app/views'));
+app.set('view engine', 'hbs');
+
+app.get('/', (req, res) => {
+  res.send({
+    title: "Meu yeye Express",
+    version: "0.0.0.0.1"
+  });
+});
+app.get('/a', (req, res) => {
+  res.render('home');
 });
 
 app.listen(port, err => {
-    console.log(`Server is listening on ${port}`);
+  console.log(`Server is listening on ${port}`);
 });
